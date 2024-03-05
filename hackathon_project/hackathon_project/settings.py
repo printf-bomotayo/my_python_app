cd287f2507034ea98e0102d81d3f0fdc4475f07e
printf-bomotayo <bomotayo2020@outlook.com>
2024-03-05 01:29:39 +0100

    adding gunicorn dependency

--- hackathon_project/hackathon_project/settings.py
+++ hackathon_project/hackathon_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tkwp-9o$y+goe674_a8!e(kc0txv(&pbjy$k=@2sa%&y3wc*i4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://robo-advisor-api.onrender.com']
 
 
 # Application definition
